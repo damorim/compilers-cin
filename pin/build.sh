aab26f7814474c91f30d74a6234f344b0dbc68a6
Marcelo d'Amorim <damorim@cin.ufpe.br>
2015-05-11 09:26:26 -0300

    minor comments

--- pin/build.sh
+++ pin/build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+OUT=${HOME}/projects/compilers-cin/pin/out
+mkdir -p ${OUT}
+
 ## I have no idea how to add an rule in the makefile to compile
 ## the tool directly. In the meantime, this script will do the job.
 
+### that is fine -Marcelo
 
 # compile our pintool
 # see this link for details: http://www.cs.swarthmore.edu/~bylvisa1/cs97/f13/pin_tutorial.html
-
-mkdir -p obj-intel64/
-make obj-intel64/memorytrace.so
+make -e ${OUT}/memorytrace.so
 
 # compile the example
-
 gcc -g test.c -o test.out
 
