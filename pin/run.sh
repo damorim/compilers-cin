c999b55ff165e2b6add624d88e76827c5f50a86f
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2015-05-14 16:41:37 -0300

    generalizing script to ubuntu 32-bit

--- pin/run.sh
+++ pin/run.sh
@@ -3,7 +3,7 @@
 ## Performs the instrumentation 
 
 # TODO share the value of this variable with the makefile
-PIN_ROOT="/home/mateus/tools/pin-2.14-71313-gcc.4.4.7-linux/"
+PIN_ROOT="/home/damorim/Tools/pin-2.14-71313-gcc.4.4.7-linux/"
 
 echo "Current PIN_ROOT:$PIN_ROOT"
 ${PIN_ROOT}/pin -t obj-intel64/memorytrace.so -i addresses.in -o trace.out -- ./test.out
