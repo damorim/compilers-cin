51ab9df7b6a8d51a5cef85b200f067bb1be4bf40
Mateus Borges <mab@cin.ufpe.br>
2015-05-15 23:04:00 -0300

    change memorytrace to handle distinct data types

--- pin/memorytrace.cpp
+++ pin/memorytrace.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <sstream>
 #include <stdlib.h>
 #include "pin.H"
 
@@ -9,7 +10,43 @@ using namespace std;
 
 fstream outfile;
 fstream infile;
-map<long,string> pointerMap;
+
+enum var_type {
+  T_INT,
+  T_WIDE,
+  T_BOOL,
+  T_CHARP,
+  T_INTP,
+  T_ENUM,
+  T_LONG,
+  T_UNK
+};
+
+struct var_info {
+  string name;
+  var_type type;
+};
+
+var_type toEnum(string type) {
+  if (type == "int") 
+	return T_INT;
+  else if (type == "int*") 
+	return T_INTP;
+  else if (type == "char*") 
+	return T_CHARP;
+  else if (type == "HOST_WIDE_INT")
+	return T_WIDE;
+  else if (type == "bool")
+	return T_BOOL;
+  else if (type == "enum")
+	return T_ENUM;
+  else if (type == "long")
+	return T_LONG;
+  else 
+	return T_UNK;
+}
+
+map<long,var_info> pointerMap;
 
 KNOB<string> KnobInputFile(KNOB_MODE_WRITEONCE, "pintool",	"i", "addresses.in", "name of the file with addresses to be watched");
 KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",	"o", "values.out", "name of the file to write the results");
@@ -17,12 +54,51 @@ KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",	"o", "values.out", "
 // Print a memory read record
 VOID RecordMemRead(VOID * ip, VOID * addr)
 {
-  map<long,string>::iterator it = pointerMap.find((long) addr);
+  map<long,var_info>::iterator it = pointerMap.find((long) addr);
   if (it != pointerMap.end()) {
-	int value;
-	//TODO handle non-integer values
-	PIN_SafeCopy(&value, addr, sizeof(int));
-	outfile << "memread: name=" << it->second << ", val=" << value << "\n";
+	ostringstream svalue;
+
+	switch (it->second.type) {
+	case T_INT:
+	case T_BOOL:
+	case T_ENUM:
+	  {
+	  int value;
+	  PIN_SafeCopy(&value, addr, sizeof(int));
+	  svalue << value;
+	  }
+	  break;
+	case T_WIDE:
+	case T_LONG:
+	  {
+	  long value;
+	  PIN_SafeCopy(&value, addr, sizeof(long));
+	  svalue << value;
+	  }
+	  break;
+	case T_CHARP:
+	  {
+	  char* pointer;
+	  PIN_SafeCopy(&pointer, addr, sizeof(char*));
+	  char value;
+	  PIN_SafeCopy(&value, pointer, sizeof(char));
+	  svalue << value;
+	  }
+	  break;
+	case T_INTP:
+	  {
+	  int* pointer;
+	  PIN_SafeCopy(&pointer, addr, sizeof(int*));
+	  int value;
+	  PIN_SafeCopy(&value, pointer, sizeof(int));
+	  svalue  << value;
+	  }
+	  break;
+	case T_UNK:
+	  svalue << "ERROR! T_UNK types should never appear at this point!";
+	  break;
+	}
+	outfile << "memread: name=" << it->second.name << ", val=" << svalue.str() << "\n";
   }
   
   //  fprintf(trace,"memread: pos=%p, val=%d\n", addr, value);
@@ -99,17 +175,31 @@ int main(int argc, char *argv[])
 
 	if (infile.is_open() && outfile.is_open()) {
 	  string address;
+	  int i = 0;
 	  while (getline(infile, address, ' ')) {
 		string name;
-		getline(infile, name);
+		string stype;
+		getline(infile, name, ' ');
+		getline(infile, stype);
 
-		//pointers are represented in hexadecimal
-		long pointerAddr = strtol(address.c_str(),NULL,16);
-		pointerMap[pointerAddr] = name;
+
+		var_type type = toEnum(stype);
+
+		if (type == T_UNK) {
+		  PIN_ERROR("Unknown type : " + stype);
+		  return -1;
+		} else {
+		  //pointers are represented in hexadecimal
+		  long pointerAddr = strtol(address.c_str(),NULL,16);
+
+		  var_info vinfo = {name,type};
+		  pointerMap[pointerAddr] = vinfo;
+		  i++;
+		}
 	  }
 
-	  for (map<long,string>::iterator it=pointerMap.begin(); it!=pointerMap.end(); ++it)
-		outfile << it->first << " => " << it->second << '\n';
+	  for (map<long,var_info>::iterator it=pointerMap.begin(); it!=pointerMap.end(); ++it)
+		outfile << it->first << " => " << it->second.name << " " << it-> second.type << '\n';
 
 	} else {
 	  PIN_ERROR( "Error opening input/output files!\n" 
--- pin/test.c
+++ pin/test.c
@@ -5,12 +5,14 @@ extern int globalVar1 = 123456;
 struct dataStruct {
   int l1;
   int l2;
+  long l3;
 };
 
-struct dataStruct globalVar2 = {234567,345678};
+struct dataStruct globalVar2 = {234567,345678,99999888};
 
 int main(void) {
   printf("value of g1: %d\n", globalVar1);
   printf("value of g2-1: %d\n", globalVar2.l1);
   printf("value of g2-2: %d\n", globalVar2.l2);
+  printf("value of g2-3: %ld\n", globalVar2.l3);
 }
