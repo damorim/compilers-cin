7e03de6c3a7cc463ab98968a363c6558f6c987c6
damorim <damorim.marcelo@gmail.com>
2016-08-25 14:28:07 -0300

    adjusting example java parser

---
+++ java-parser/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //        ParserExample.printCompilationUnit("src/main/java/BinarySearchTree.java");
+        //        ParserExample.listMethods("src/main/java/BinarySearchTree.java");
+        ParserExample.buildUnit();
+    }
+}
