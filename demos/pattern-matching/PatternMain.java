194f3e10f6b03945a567d639e310a6b72eae6ed0
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2018-08-30 14:57:14 -0300

    minor change

--- demos/pattern-matching/PatternMain.java
+++ demos/pattern-matching/PatternMain.java
@@ -5,8 +5,11 @@ public class PatternMain {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("([a-z]*)([0-9]*)");
         Matcher matcher = pattern.matcher(args[0]);
-        matcher.matches();
-        System.out.println(matcher.group(1));
-        System.out.println(matcher.group(2));        
+        if (matcher.matches()) {
+          System.out.println(matcher.group(1));
+          System.out.println(matcher.group(2));
+        } else {
+          System.err.println("No match!");
+        }
     }
 }
