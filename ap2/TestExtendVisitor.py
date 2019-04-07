910356c8777d8d62d598bbfc7ae3be53ec0ed8d6
marcomnt <mpmr@cin.ufpe.br>
2019-04-06 21:23:38 -0300

    Create TestExtendVisitor.py

---
+++ ap2/TestExtendVisitor.py
@@ -0,0 +1,26 @@
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .autogen.TestParser import TestParser
+else:
+    from autogen.TestParser import TestParser
+
+from autogen.TestVisitor import TestVisitor
+
+class TestExtendVisitor(TestVisitor):
+	def visitExpressions(self, ctx:TestParser.ExpressionsContext):
+		return self.visitChildren(ctx)
+
+	def visitAssign(self, ctx:TestParser.AssignContext):
+		print("ZA WARUDO",ctx.ID())
+		return self.visitChildren(ctx)
+
+	# Visit a parse tree produced by TestParser#AddSub.
+	def visitAddSub(self, ctx:TestParser.AddSubContext):
+		left = self.visit(ctx.expr()[0])
+		right= self.visit(ctx.expr()[1])
+		print("TOKI O TOMARE  ",left,"   ", right)
+		print("KONO DIO DA ", str(int(left)+int(right) ) )
+		return self.visitChildren(ctx)
+
+	def visitInt(self, ctx:TestParser.IntContext):
+		return int(ctx.INT().getText())
