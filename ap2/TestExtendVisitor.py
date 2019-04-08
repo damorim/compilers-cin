3edfecfd22546e91170ed33e4c6fe4ddd8e28cbf
Felipe Cavalcanti Guerra Ramos <fcgr@uhwc20.windows.cin.ufpe.br>
2019-04-08 09:50:42 -0300

    test melhorado

--- ap2/TestExtendVisitor.py
+++ ap2/TestExtendVisitor.py
@@ -3,9 +3,12 @@ from autogen.TestParser import TestParser
 from autogen.TestVisitor import TestVisitor
 
 class TestExtendVisitor(TestVisitor):
+
+	# Visit a parse tree produced by TestParser#Expressions.
 	def visitExpressions(self, ctx:TestParser.ExpressionsContext):
 		return self.visitChildren(ctx)
 
+	# Visit a parse tree produced by TestParser#Assign.
 	def visitAssign(self, ctx:TestParser.AssignContext):
 		print("ZA WARUDO",ctx.ID())
 		return self.visitChildren(ctx)
@@ -15,8 +18,25 @@ class TestExtendVisitor(TestVisitor):
 		left = self.visit(ctx.expr()[0])
 		right= self.visit(ctx.expr()[1])
 		print("TOKI O TOMARE  ",left,"   ", right)
-		print("KONO DIO DA ", str(int(left)+int(right) ) )
+		print(ctx.op.text)
+		if (ctx.op.text == '+'):
+			print("KONO DIO DA ", str(int(left) + int(right)))
+		else:
+			print("KONO DIO DA ", str(int(left) - int(right)))
 		return self.visitChildren(ctx)
 
+	# Visit a parse tree produced by TestParser#Int.
 	def visitInt(self, ctx:TestParser.IntContext):
 		return int(ctx.INT().getText())
+	
+	# Visit a parse tree produced by TestParser#Identifier.
+	def visitIdentifier(self, ctx:TestParser.IdentifierContext):
+		return self.visitChildren(ctx)
+
+	# Visit a parse tree produced by TestParser#Paren.
+	def visitParen(self, ctx:TestParser.ParenContext):
+		return self.visitChildren(ctx)
+
+	# Visit a parse tree produced by TestParser#MultDiv.
+	def visitMultDiv(self, ctx:TestParser.MultDivContext):
+		return self.visitChildren(ctx) 
