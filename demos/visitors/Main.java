e9b332d4d544b61c829459718b19438bbc28fd91
Marcelo d'Amorim <damorim.marcelo@gmail.com>
2019-09-03 16:25:08 -0300

    adding another visitor example

--- demos/visitors/Main.java
+++ demos/visitors/Main.java
@@ -3,7 +3,19 @@ public class Main {
         // assume this is done by the parser
         Expr childExpr = new BinaryExpr("+", new LiteralExpr(5), new LiteralExpr(3));
         Expr rootExpr = new BinaryExpr("*", childExpr, new LiteralExpr(2));
+        //        example_postfix(rootExpr);
+        example_calculator(rootExpr);        
+    }
+
+    private static void example_postfix(Expr rootExpr) {
         Visitor vis = new PrinterPosfix();
         rootExpr.accept(vis);
     }
+
+    private static void example_calculator(Expr rootExpr) {
+        CalculatorVisitor vis = new CalculatorVisitor();
+        rootExpr.accept(vis);
+        System.out.println("--> "+vis.st.pop());
+    }
+    
 }
