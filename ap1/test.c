9b8bf92a409eec7d5121de847dc3c778f556fed8
Felipe Guerra <felipe.cgr@hotmail.com>
2019-09-05 11:03:14 -0300

    test.c

--- ap1/test.c
+++ ap1/test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int scalar (int a, int b) {
 	return sqrt(a * a + b * b - a * b);
 }
 
 int main () {
-	int a = 3, b = 8;
-	printf("scalar(%d, %d) = %d\n", a, b, scalar(a, b));
+	int quantas_trincas = 33, valor1 = 821;
+	printf("scalar(%d, %d) = %d\n", quantas_trincas, valor1, scalar(quantas_trincas, valor1));
 	return 0;
 }
