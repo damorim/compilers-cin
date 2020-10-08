449f0d0495cb166a2e358eb4ada9888ee7b0897e
Felipe Guerra <felipe.cgr@hotmail.com>
2020-10-07 23:46:56 -0300

    projeto2 pronto

--- 2020_3/projeto2/GrammarCheckerVisitor.py
+++ 2020_3/projeto2/GrammarCheckerVisitor.py
@@ -1,61 +1,147 @@
+# Generated from antlr4-python3-runtime-4.7.2/src/autogen/Grammar.g4 by ANTLR 4.7.2
 from antlr4 import *
-from autogen.GrammarParser import GrammarParser 
-from autogen.GrammarVisitor import GrammarVisitor
+if __name__ is not None and "." in __name__:
+    from .GrammarParser import GrammarParser
+else:
+    from GrammarParser import GrammarParser
 
+# This class defines a complete generic visitor for a parse tree produced by GrammarParser.
+
+'''
+COMO RESGATAR INFORMAÇÕES DA ÁRVORE
+
+Observe o seu Grammar.g4. Cada regra sintática gea uma função com o nome corespondente no Visitor e na ordem em que está na gramática. Para deixar na ordem em que você colocou, substitua as funções dentro de "class GrammarCheckerVisitor(ParseTreeVisitor)" pelas que apareem em autogem/GrammarVisitor.g4 depois do make ser rodado. Mas antes algumas mudanças precisam ser feitas em Grammar.g4. Primeiro copie-a do projeto1 para o projeto2. Depois adicione o tipo "VOID: 'void'" no lexer e na regra de "type" no parser (só "| VOID" aqui). Agora, por causa de conflitos com Python, substitua as regras file por fiile e type por type ("make adjust" substitui automaticamente). Use prints temporários para ver se está no caminho certo.  "make tree" agora desenha a árvore sintática, se quiser vê-la para qualquer input, enquanto "make" roda este visitor sobre o a árvore gerada a partir de Grammar.g4 alimentada pelo input.
+
+
+expr = ctx.expression().accept(self)  # entra no nó expression e seus filhos e retorna alguma coisa
+
+for i in range(len(ctx.identifier())): # para cada identficador que este nó possui...
+    ident = ctx.identifier()[i].accept(self) # ...pegue o i-ésimo
+
+if ctx.FLOAT() != None: # se houver um FLOAT (em vez de INT ou VOID) neste nó (parser)
+    return Type.FLOAT # retorne float
+'''
+
+# retorne Type.INT, etc para fazer checagem de tipos
 class Type:
-	VOID = "void"
-	INT = "int"
+    VOID = "void"
+    INT = "int"
+    FLOAT = "float"
+
+class GrammarCheckerVisitor(ParseTreeVisitor):
+    ids_defined = {} # armazenar informações necessárias para cada identifier definido
+
+    # Visit a parse tree produced by GrammarParser#fiile.
+    def visitFiile(self, ctx:GrammarParser.FiileContext):
+        return self.visitChildren(ctx)
 
 
-class GrammarCheckerVisitor(GrammarVisitor):
-	id_values = {}
-
-	def visitIntExpr(self, ctx:GrammarParser.IntExprContext):
-		print("visting "+Type.INT)
-		return Type.INT
+    # Visit a parse tree produced by GrammarParser#function_definition.
+    def visitFunction_definition(self, ctx:GrammarParser.Function_definitionContext):
+        return self.visitChildren(ctx)
 
 
-
-	def visitVarDecl(self, ctx:GrammarParser.VarDeclContext):
-		var_name = ctx.ID().getText()
-		tyype = ctx.tyype().getText()
-		print("tyype = " + tyype)
-		
-		if (tyype == Type.VOID):
-			result = Type.VOID
-			print("Mensagem de erro 1...")
-			exit(1)
-		else:
-			if ctx.expr() != None:
-				init = ctx.expr().accept(self)
-				print("init = " + init)
-				if init != tyype:
-					print("Mensagem de erro 2...")
-					exit(2)
-
-			result = tyype
-			self.id_values[var_name] = tyype
-
-		print("saved variable " + var_name + " of type " + tyype)
-		return result
+    # Visit a parse tree produced by GrammarParser#body.
+    def visitBody(self, ctx:GrammarParser.BodyContext):
+        return self.visitChildren(ctx)
 
 
-
-	def visitAddSubExpr(self, ctx:GrammarParser.AddSubExprContext):
-		left = ctx.expr()[0].accept(self)
-		right = ctx.expr()[1].accept(self)
-
-		if left == Type.INT and right == Type.INT:
-			result = Type.INT
-		else:
-			reult = Type.VOID
-			print("Mensagem de erro 3...")
-			exit()
-		
-		print("addition or subtraction of " + left + " " + right + " that results in a " + result)
-		return result
+    # Visit a parse tree produced by GrammarParser#statement.
+    def visitStatement(self, ctx:GrammarParser.StatementContext):
+        return self.visitChildren(ctx)
 
 
+    # Visit a parse tree produced by GrammarParser#if_statement.
+    def visitIf_statement(self, ctx:GrammarParser.If_statementContext):
+        return self.visitChildren(ctx)
 
-	def aggregateResult(self, aggregate:Type, next_result:Type):
-		return next_result if next_result != None else aggregate
+
+    # Visit a parse tree produced by GrammarParser#else_statement.
+    def visitElse_statement(self, ctx:GrammarParser.Else_statementContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#for_loop.
+    def visitFor_loop(self, ctx:GrammarParser.For_loopContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#for_initializer.
+    def visitFor_initializer(self, ctx:GrammarParser.For_initializerContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#for_condition.
+    def visitFor_condition(self, ctx:GrammarParser.For_conditionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#for_step.
+    def visitFor_step(self, ctx:GrammarParser.For_stepContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#variable_definition.
+    def visitVariable_definition(self, ctx:GrammarParser.Variable_definitionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#variable_assignment.
+    def visitVariable_assignment(self, ctx:GrammarParser.Variable_assignmentContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#expression.
+    def visitExpression(self, ctx:GrammarParser.ExpressionContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#array.
+    def visitArray(self, ctx:GrammarParser.ArrayContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#array_literal.
+    def visitArray_literal(self, ctx:GrammarParser.Array_literalContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#function_call.
+    def visitFunction_call(self, ctx:GrammarParser.Function_callContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#arguments.
+    def visitArguments(self, ctx:GrammarParser.ArgumentsContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#tyype.
+    def visitTyype(self, ctx:GrammarParser.TyypeContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#integer.
+    def visitInteger(self, ctx:GrammarParser.IntegerContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#floating.
+    def visitFloating(self, ctx:GrammarParser.FloatingContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#string.
+    def visitString(self, ctx:GrammarParser.StringContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by GrammarParser#identifier.
+    def visitIdentifier(self, ctx:GrammarParser.IdentifierContext):
+        return self.visitChildren(ctx)
+
+
+    #del GrammarParser
+
+    #def aggregateResult(self, aggregate:Type, next_result:Type):
+        #return next_result if next_result != None else aggregate
