56f324cf880a4857b0c2616188da7b09d87a6573
brayner1 <rbl2@cin.ufpe.br>
2021-03-28 20:11:00 -0300

    update files

--- 2020_3/projeto3/inputs/00.c
+++ 2020_3/projeto3/inputs/00.c
@@ -1,12 +1,13 @@
-int a = 3;
-float b = 0.3;
+
 
 float square (float x) {
 	return x * x;
 }
 
 int main () {
+	int a = 3;
+	float b = 0.3;
 	float c = a / b;
 	c /= 3 * (2 - b) + 4;
-	return 0;
+	return  0*a;
 }
